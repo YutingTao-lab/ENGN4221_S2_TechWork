1d08e28979e18d83996f1c477017c3ccaa41f4f4
YutingTao-lab <71167152+YutingTao-lab@users.noreply.github.com>
2020-09-20 17:48:35 +1000

    Add files via upload

---
+++ main_program.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 17:29:46 2020
+
+@author: ziling
+"""
+
+
+import step_evaluation1
+import four_loops_arrange
+import sympy
+import Data_Estimation
+import time_range
+import pandas as pd
+import  xdrlib ,sys
+import xlrd
+import xlwt
+import math
+from xlutils.copy import copy
+
+
+def get_ideal_range():
+    # h = input("Please enter your filepath: ")
+    # y = input("Please enter your sheet name:")
+    Data_Estimation.excel_table_byname()
+    time_range.excel_table_byname(file= 'Data_estimation.xls', colnameindex=0, by_name=u'sheet1')
+
+def data_step():
+    #get the analyzable steps
+    step_ana1 = step_evaluation1.excel_table(file= 'Data_estimation.xls', colnameindex=0, by_name=u'sheet1')
+    newdata = xlrd.open_workbook('new_dataset.xls')
+    step_ana = newdata.sheet_by_name('new_dataset')
+    key = step_ana.col_values(0)[1:]
+    n = 1
+    dict_data = {}
+    for i in key:
+        #m = 1
+        value =[]
+        # print(step_ana.row_values(1))
+        for j in range(0,len(step_ana.row_values(0)[1:])):
+            m = float(step_ana.row_values(n)[j+1])
+            # print(m)
+            value.insert(j,m)
+            # print(m,value)
+            #m = m+1
+            dict_anadata = {i:value}
+        dict_data.update(dict_anadata)
+        #print(dict_anadata)
+        n = n+1
+    return dict_data
+
+def low_var_data():
+    time_lmax = xlrd.open_workbook('ideal_range.xls').sheet_by_name('sheet1').col_values(1)[1:]
+    return time_lmax
+
+def High_var_data():
+    time_hmax = xlrd.open_workbook('ideal_range.xls').sheet_by_name('sheet1').col_values(2)[1:]
+    return time_hmax
+
+def loop_and_steps():
+    all_steps = xlrd.open_workbook('Data_estimation.xls').sheet_by_name('sheet1').col_values(0)
+    dict_loops={}
+    # dict_loops = {'loopA':all_steps[0:2],'loopB':all_steps[2:9],'loopC':all_steps[9:13],'loopD':all_steps[13:]}
+    for i in all_steps[0:2]:
+        key = i
+        dict_steps={key:'A'}
+        dict_loops.update(dict_steps)
+    for i in all_steps[2:9]:
+        key = i
+        dict_steps={key:'B'}
+        dict_loops.update(dict_steps)
+    for i in all_steps[9:13]:
+        key = i
+        dict_steps={key:'C'}
+        dict_loops.update(dict_steps)
+    for i in all_steps[13:]:
+        key = i
+        dict_steps={key:'D'}
+        dict_loops.update(dict_steps)
+    return dict_loops
+
+def define_error_data():    
+    get_ideal_range()
+    time_lmax = low_var_data()
+    time_hmax = High_var_data()
+    dict_data=data_step()
+    dict_loops = loop_and_steps()
+    steps=list(dict_data.keys())
+    steps_values = list(dict_data.values())
+    dict_loops=loop_and_steps()
+    maxloop = four_loops_arrange.excel_table_byname(file= 'Data_estimation.xls', colnameindex=0, by_name=u'sheet1')
+    time_set1 = xlrd.open_workbook('ideal_range.xls')
+    
+    #create new sheet to describe the type of steps
+    time_range1 = time_set1.sheet_by_name('sheet1')
+    time_set = copy(time_set1)
+    step_type = time_set.add_sheet('step_type')
+    step_type.write(0,0,'improvable steps')
+    step_type.write(0,1,'step types')
+    time_set.save('ideal_range.xls')
+    for i in range(0,len(steps)):
+        step_type.write(i+1,0,steps[i])
+        time_set.save('ideal_range.xls')
+    
+    #Evaluate and classify steps that can be improved
+    r = 0
+    for j in range(0,len(steps)):
+        r = r+1
+        j_steps = steps[j]
+        lmax = time_lmax[j]
+        hmax = time_hmax[j]
+        count1=0
+        count2=0
+        for i in steps_values[j]:
+            if i >= lmax:
+                if i < hmax:
+                    count1 = count1+1
+                else:
+                    count2 = count2+1
+        if count1+count2 <= 0.1*len(steps_values[j]):
+            step_type.write(r,1,'type N')
+            time_set.save('ideal_range.xls')
+            print('Step',j_steps,'is type N')
+        else:
+            if count2 != 0:
+                if count2>=count1:
+                    if dict_loops[j_steps] == maxloop:
+                        step_type.write(r,1,'type E')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type E')
+                    else:
+                        step_type.write(r,1,'type F')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type F')
+                else:
+                    if dict_loops[j_steps] == maxloop:
+                        step_type.write(r,1,'type G')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type G')
+                    else:
+                        step_type.write(r,1,'type H')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type H')
+            else:
+                if count1>=0.5*len(steps_values[j]):
+                    if dict_loops[j_steps] == maxloop:
+                        step_type.write(r,1,'type A')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type A')
+                    else:
+                        step_type.write(r,1,'type B')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type B')
+                else:
+                    if dict_loops[j_steps] == maxloop:
+                        step_type.write(r,1,'type C')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type C')
+                    else:
+                        step_type.write(r,1,'type D')
+                        time_set.save('ideal_range.xls')
+                        print('Step',j_steps,'is type D')
+    # return error
+    
+if __name__ =="__main__":
+    define_error_data()
---
+++ time_range.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 17:26:56 2020
+
+@author: ziling
+"""
+
+import sympy
+import pandas as pd
+import  xdrlib ,sys
+import xlrd
+import xlwt
+import numpy as np
+
+from scipy.stats import t
+from numpy import average, std
+from math import sqrt
+
+
+file = 'Data_estimation.xls'
+def open_excel(file= 'Data_estimation.xls'):
+    try:
+        data = xlrd.open_workbook(file)
+        return data
+    except Exception:
+        print (str(e))
+
+def set_style(name,height,bold=False):
+	style = xlwt.XFStyle()
+	font = xlwt.Font()
+	font.name = name
+	font.bold = bold
+	font.color_index = 4
+	font.height = height
+	style.font = font
+	return style
+
+def excel_table_byname(file= 'Data_estimation.xls', colnameindex=0, by_name=u'sheet1'):
+    data = open_excel(file) #Open excel
+    table = data.sheet_by_name(by_name)#obtain the sheet in Excel file by name
+    book = xlwt.Workbook() #create Excel file
+    sheet1 = book.add_sheet('sheet1')
+    
+    col0=table.col_values(0)
+    for i in range(0,len(col0)):
+        sheet1.write(i,0,str(col0[i]))
+        book.save('ideal_range.xls')
+    
+    row0 = ['lmin','lmax','hmin','hmax']
+    for j in range(0,len(row0)):
+        sheet1.write(0,j+1,str(row0[j]))
+        book.save('ideal_range.xls')
+#input the raw data as a matrix
+    set_matrix=[]
+    for row in range (1,table.nrows):
+        _row = []
+        for col in range (1,table.ncols-8):
+            _row.append(table.cell_value(row,col+1))
+        set_matrix.append(_row)
+    set_matrix_array = np.array(set_matrix)
+
+    #sample mean
+    mean = table.col_values(-6)
+    mean.pop(0)
+
+    #sample variance (The degree used in calculations is N - ddof), the calculation is done in Excel
+    stddev = table.col_values(-4)
+    stddev.pop(0)
+
+    [h,l] = set_matrix_array.shape
+    #due to the small sample size, using t-distribution, the confidence level is 95%
+    t_bounds = t.interval(0.9, l - 1,mean,stddev)
+    t_bounds = np.vstack(t_bounds)
+    t_bounds = t_bounds.transpose()
+    [a, b] = t_bounds.shape
+    for i in range(a):
+        for j in range(b):
+            sheet1.write(i+1, j+1, t_bounds[i, j])
+            book.save('ideal_range.xls')
+        
+        
+        
+        
+        
+# if __name__ =="__main__":
+#   excel_table_byname(file= 'Data_estimation.xls', colnameindex=0, by_name=u'sheet1')
